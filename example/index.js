450cbbf7c87d23509468f3c95bd98529dbde08b8
jjhlzn <jinjunhang@hotmail.com>
2017-08-30 08:57:30 +0800

    首页加载优化

--- example/index.js
+++ example/index.js
@@ -14,9 +14,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+     /* 
       wx.redirectTo({
         url: './login/login',
-      })
+      }) */
+
+      let loginUser = wx.getStorageSync('loginUser');
+      console.log('loginUser: ', loginUser);
+      if (loginUser) {
+        wx.reLaunch({
+          url: './notchecklist/notchecklist',
+        });
+      } else {
+        wx.redirectTo({
+          url: './login/login',
+        })
+      }
   },
 
   /**
--- example/login/login.js
+++ example/login/login.js
@@ -14,17 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    /*
-    wx.reLaunch({
-      url: '../notchecklist/notchecklist',
-    }); */
-    let loginUser = wx.getStorageSync('loginUser');
-    console.log('loginUser: ', loginUser);
-    if (loginUser) {
-      wx.reLaunch({
-        url: '../notchecklist/notchecklist',
-      });
-    }
+
   },
 
   /**
